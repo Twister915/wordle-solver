9ff279d46349a06f2aeb26db843bc191c428760e
Joey Sacchini <joey@sacchini.net>
2022-06-14 12:09:11 -0400

    update build script to be a bit cleaner

--- build.rs
+++ build.rs
@@ -1,51 +1,77 @@
-use std::{io, process};
+use std::{io, process, fmt};
 
 const DEFAULT_VERSION: &str = "???";
 
 fn main() {
-    let cmd_result = process::Command::new("git")
-        .args(&["rev-parse", "--short", "HEAD"])
-        .output();
+    println!("cargo:rustc-env=GIT_HASH={}", determine_git_version());
+}
 
-    let git_hash = handle_cmd_git_output(cmd_result);
-
-    println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+fn determine_git_version() -> String {
+    handle_cmd_git_output(
+        process::Command::new("git")
+            .args(&["rev-parse", "--short", "HEAD"])
+            .output())
 }
 
 fn handle_cmd_git_output(input: io::Result<process::Output>) -> String {
     match input {
-        Err(err) => {
-            println!("cargo:warning=unable to execute git command... {:?}", err);
-        },
         Ok(output) => {
+            // check exit status from git, should be 0
             let is_exit_code_ok = output.status.success();
             if !is_exit_code_ok {
-                println!("cargo:warning=got non-0 exit code... {}", output.status.to_string());
+                warning(format_args!("got non-0 exit code... {}", output.status.to_string()));
             }
 
+            // try to read stderr...
             let mut had_err = false;
-            if let Some(err_out) = String::from_utf8(output.stderr).ok() {
+            if let Some(err_out) = process_command_output_bytes("stderr", output.stderr) {
+                // if there's some data in stderr, and it's non-empty, then we should print that and
+                // assume the command failed...
                 if !err_out.trim().is_empty() {
-                    println!("cargo:warning={}", err_out);
+                    warning(format_args!("{}", err_out));
                     had_err = true;
                 }
             }
 
+            // if exit status is 0 && nothing was in stderr, then we can process stdout
             if is_exit_code_ok && !had_err {
-                if let Some(out) = String::from_utf8(output.stdout).ok() {
+                // try to interpret stdout
+                if let Some(out) = process_command_output_bytes("stdout", output.stdout) {
+                    // verify that the "cleaned up" version of the git hash is non-empty (and therefore valid)
                     let cleaned_up = out.trim().to_ascii_lowercase();
                     if cleaned_up.is_empty() {
-                        println!("cargo:warning=no version returned from git??");
+                        warning(format_args!("no version returned from git??"))
                     } else {
+                        // this branch is only reached if all checks pass
+                        // we return the actual git version returned by the git command
                         return cleaned_up;
                     }
-                } else {
-                    println!("cargo:warning=failed to interpret stdout, not utf8?");
                 }
             }
         }
+        Err(err) => {
+            warning(format_args!("unable to execute git command... {:?}", err));
+        }
     }
 
-    println!("cargo:warning=unable to determine version... using default version '{}'", DEFAULT_VERSION);
+    // if any checks fail above, then this section will be reached (because only one other return
+    // is present in this function, in the "all checks passed" path)
+    //
+    // we write a warning & return a default version string
+    warning(format_args!("unable to determine version... using default version '{}'", DEFAULT_VERSION));
     DEFAULT_VERSION.to_string()
+}
+
+fn process_command_output_bytes(name: &str, b: Vec<u8>) -> Option<String> {
+    match String::from_utf8(b) {
+        Ok(str) => Some(str),
+        Err(err) => {
+            warning(format_args!("failed to read {} output (not utf8??)... err={:?}", name, err));
+            None
+        }
+    }
+}
+
+fn warning(args: fmt::Arguments<'_>) {
+    println!("cargo:warning={}", args)
 }
