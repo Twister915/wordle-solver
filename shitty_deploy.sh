926fc20445d260c758cda211ace881b069f1dc69
Joey Sacchini <joey@sacchini.net>
2022-06-28 09:49:08 -0400

    update gitignore to ignore deploy scripts

--- shitty_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-rm -fr target/ dist/
-
-trunk build --release
-
-scp -r dist/* joey@mgk1.joey.sh:~/wordle-html/;
--- shitty_deploy_qa.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-trunk build --release
-
-scp -r dist/* joey@mgk1.joey.sh:~/wordle-qa-html/;
