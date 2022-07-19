72f67419d99471d04866d9a6b770277aa5f3bc9a
Joey Sacchini <joey@sacchini.net>
2022-07-19 12:21:51 -0400

    remove wee_alloc (not maintained, has leaks)

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -30,8 +30,8 @@ pub const LOG_LEVEL: log::Level = log::Level::Debug;
 #[cfg(not(debug_assertions))]
 pub const LOG_LEVEL: log::Level = log::Level::Info;
 
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+// #[global_allocator]
+// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 pub fn main() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
